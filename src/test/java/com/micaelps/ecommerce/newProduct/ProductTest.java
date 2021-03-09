a06008a8a9d0a9c66466be8cb8a5b7b6df8bf78d
micaelps <3.1416micael@gmail.com>
2021-03-09 15:07:28 -0300

    tests: add decrease inventory

--- src/test/java/com/micaelps/ecommerce/newProduct/ProductTest.java
+++ src/test/java/com/micaelps/ecommerce/newProduct/ProductTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -69,5 +71,35 @@ class ProductTest {
                         )
                 )
         );
+    }
+    @DisplayName("Should not decrease the stock if the order for greater than the quantity in stock")
+    @ParameterizedTest
+    @CsvSource({"1,1,true","1,2,false","4,2,true","1,5, false"})
+    void decrease_product_inventary(int inventary, int quantity, boolean expectedResult){
+        List<AttributeRequest> attributes = new ArrayList<>();
+        attributes.add(new AttributeRequest("key1","value1"));
+        attributes.add(new AttributeRequest("key2","value2"));
+        attributes.add(new AttributeRequest("key3","value3"));
+
+        Category category = new Category("geral");
+        UserSystem userSystem = new UserSystem("m@email.com","123456");
+        Product product = new Product("nome",BigDecimal.TEN, inventary, attributes,"aaaa", category, userSystem);
+        boolean result = product.decreaseInvetory(quantity);
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @DisplayName("Should throws exception for invalid quantity")
+    @ParameterizedTest
+    @CsvSource({"0","-1","-100"})
+    void throw_illegalArgumentException_decrease_inventory(int quantity) throws Exception{
+        List<AttributeRequest> attributes = new ArrayList<>();
+        attributes.add(new AttributeRequest("key1","value1"));
+        attributes.add(new AttributeRequest("key2","value2"));
+        attributes.add(new AttributeRequest("key3","value3"));
+
+        Category category = new Category("geral");
+        UserSystem userSystem = new UserSystem("m@email.com","123456");
+        Product product = new Product("nome",BigDecimal.TEN, 10, attributes,"aaaa", category, userSystem);
+        Assertions.assertThrows(IllegalArgumentException.class, ()->product.decreaseInvetory(quantity));
     }
 }
